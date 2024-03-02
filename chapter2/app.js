1dbd91a1d66dcfc52f5e1198192946528bc796ec
bilalsalani <161963664+bilalsalani@users.noreply.github.com>
2024-03-02 21:55:53 +0500

    Add files via upload

---
+++ chapter2/app.js
@@ -0,0 +1,27 @@
+var username = "bilal" ;
+console.log(username)  ;
+
+var message = "Hello  World" ;
+alert(message) ;
+
+var name = "bilal"
+var age = 15 ;
+var profession = "web developer" ;
+alert(name);
+alert(age);
+alert(profession); 
+
+var food =("  pizza \n  pizz\n  piz\n  pi\n  p\n  ");
+alert(food)
+
+var email = "bilal@gmail.com" ;
+alert("my email is " + email)
+
+var book = "A smarter way to learn JavaScript" ;
+alert("I am trying to learn from the book " + book)
+
+var what =  " Yah! I can write HTML content through Javascript " ;
+document.write(what) 
+
+var show = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬" ;
+alert(show)
