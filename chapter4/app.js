f072a2428a3c266ac1ca7fdf5a390c9b5c16d902
bilal <161963664+bilalsalani@users.noreply.github.com>
2024-03-08 18:56:08 +0500

    Add files via upload

---
+++ chapter4/app.js
@@ -0,0 +1,19 @@
+/*var variable1="bilal" , variable2=true , variable3 = 32 ;
+  document.write(variable2)  ;
+
+  var leagl1= "myvar" ,
+        legal2 = username,
+        legal3 = _count ,
+        $legal1 = "$totalamount";
+document.write($legal1);*/
+
+        
+var statement1 = "variable name can only coantain numbers $ and _ for example ",
+ $name = "$name , _name or name",
+ tyo = " case sensitive",
+ statement2 = "<br> variables names are ",
+ final2 =  statement2 + tyo,
+ final1 = statement1 + $name ;
+
+ document.write(final1 ,  final2);
+
